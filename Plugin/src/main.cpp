7932ce6e7cadc6c16b4df99a4723243e52b2481d
igromanru <igromanru@yahoo.de>
2023-10-10 10:52:03 +0200

    Make ASI version, but without the ini, so it should work with the GamePass version

--- Plugin/src/main.cpp
+++ Plugin/src/main.cpp
@@ -1,7 +1,6 @@
-
 #include "PCH.h"
 
-namespace ShipCharCount
+namespace
 {
     struct Prolog : Xbyak::CodeGenerator
     {
@@ -18,8 +17,8 @@ namespace ShipCharCount
     {
         Epilog()
         {
-            // write the return value from the hook function to r8d
-            mov(r8d, eax);
+            // max name length value to r8d
+            mov(r8d, 250);
 
             // restore rax from stack
             pop(rax);
@@ -28,97 +27,64 @@ namespace ShipCharCount
         }
     };
 
-    int Hook_GetMaxCharCount()
+    void Hook_GetMaxCharCount()
     {
-        return Settings::GetSingleton()->GetShipNameMaxChars();
+        // No Settings in the ASI version
     }
 
-    void Install()
+    bool Install()
     {
-        auto patchAddress = reinterpret_cast<uintptr_t>(search_pattern<"48 8B 88 ?? ?? ?? ?? 44 89 81 ?? ?? ?? ?? C3">());
-        if (patchAddress) {
+        auto patchAddress = reinterpret_cast<uintptr_t>(Assembly::search_pattern<"48 8B 88 ?? ?? ?? ?? 44 89 81 ?? ?? ?? ?? C3">());
+        if (patchAddress)
+        {
             patchAddress += 7;
-            INFO("Found the patch address: {:x}", patchAddress);
+            INFO("Found patch address: {:x}. Game base: {:x}", patchAddress, Module::get().base());
+
+            Trampoline::AllocTrampoline(128);
 
             Prolog prolog{};
             prolog.ready();
             Epilog epilog{};
             epilog.ready();
-
-            // Create hook
+            
             const auto caveHookHandle = AddCaveHook(
                 patchAddress,
-                { 0, 7 },
+                { 0, 7 }, 
                 FUNC_INFO(Hook_GetMaxCharCount),
                 &prolog,
                 &epilog,
                 HookFlag::kRestoreAfterEpilog);
             caveHookHandle->Enable();
-            INFO("Max character count patched successfully!")
+            INFO("Patch applied");
+            return true;
         }
-        else {
-            ERROR("Couldn't find the patch address");
+        else
+        {
+            ERROR("Couldn't find the address to patch");
         }
+        return false;
     }
-}
+} // namespace
 
-DLLEXPORT constinit auto SFSEPlugin_Version = []() noexcept {
-	SFSE::PluginVersionData data{};
-
-	data.PluginVersion(Plugin::Version);
-	data.PluginName(Plugin::NAME);
-	data.AuthorName(Plugin::AUTHOR);
-	data.UsesSigScanning(true);
-	//data.UsesAddressLibrary(true);
-	data.HasNoStructUse(true);
-	//data.IsLayoutDependent(true);
-	data.CompatibleVersions({ 
-        SFSE::RUNTIME_SF_1_7_23,
-		SFSE::RUNTIME_SF_1_7_29,
-		SFSE::RUNTIME_LATEST
-	});
-
-	return data;
-}();
-
-namespace
+DWORD WINAPI Thread(LPVOID param)
 {
-	void MessageCallback(SFSE::MessagingInterface::Message* a_msg) noexcept
-	{
-		switch (a_msg->type) {
-		case SFSE::MessagingInterface::kPostLoad:
-			{
-                // Load the settings file
-                Settings::GetSingleton()->Load();
-                // Install plugin
-                ShipCharCount::Install();
-				break;
-			}
-		default:
-			break;
-		}
-	}
+    // Settings::GetSingleton()->Load();
+    return Install();
 }
 
 
-DLLEXPORT bool SFSEAPI SFSEPlugin_Load(const SFSE::LoadInterface* a_sfse)
+BOOL APIENTRY DllMain(HMODULE a_hModule, DWORD a_dwReason, LPVOID a_lpReserved)
 {
+    if (a_dwReason == DLL_PROCESS_ATTACH)
+    {
 #ifndef NDEBUG
-    MessageBoxW(NULL, L"Loaded. You can attach the debugger now, then press OK", L"Starfield-LongerNames SFSE Plugin", NULL);
-	/*while (!IsDebuggerPresent()) {
-		Sleep(100);
-	}*/
+        MessageBoxA(NULL, "Loaded. You can attach the debugger now", "SF LongerNames ASI Plugin", NULL);
 #endif
+        dku::Logger::Init(Plugin::NAME, std::to_string(Plugin::Version));
+        INFO("Game: {}", dku::Hook::GetProcessName());
 
-	SFSE::Init(a_sfse, false);
+        CloseHandle(CreateThread(nullptr, 0, Thread, nullptr, 0, nullptr));
+    }
 
-	DKUtil::Logger::Init(Plugin::NAME, std::to_string(Plugin::Version));
-
-	INFO("{} v{} loaded", Plugin::NAME, Plugin::Version);
-
-	// Insert plugin to messaging interface
-	SFSE::AllocTrampoline(1 << 8);
-	SFSE::GetMessagingInterface()->RegisterListener(MessageCallback);
-
-	return true;
+    return TRUE;
 }
